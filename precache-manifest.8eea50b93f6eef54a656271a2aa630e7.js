3235bdeab12bfb06daceea34e0a67a0e0cca27f3
Caroline Moura <54120284+CahMoura@users.noreply.github.com>
2020-06-17 15:27:19 -0300

    Updates

---
+++ precache-manifest.8eea50b93f6eef54a656271a2aa630e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6227590e18cfa7978ae9dd1f0ff211f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f86104977abc9042728",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "50eac1744f1335f053ad",
+    "url": "/static/js/2.96e5e7e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.96e5e7e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f86104977abc9042728",
+    "url": "/static/js/main.3a132981.chunk.js"
+  },
+  {
+    "revision": "6981919806c089cef906",
+    "url": "/static/js/runtime-main.bfca2edd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
